02669cf4163733a55930573d7579d6ce60afa278
Michael Fei <Yangyi.Fei@morganstanley.com>
2014-07-11 10:22:59 +0800

    Initial Commit

--- CycleProgressView/ViewController.m
+++ CycleProgressView/ViewController.m
@@ -8,22 +8,33 @@
 
 #import "ViewController.h"
 
+#import "MSCycleProgressView/MSCycleProgressView.h"
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MSCycleProgressView *progressView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
+- (void) viewDidAppear:(BOOL)animated
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.progressView.tintColor = [UIColor redColor];
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.progressView.progress = 0.5;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.progressView.progress = 0.7;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.progressView.progress = 1.0;
+    });
 }
 
 @end
